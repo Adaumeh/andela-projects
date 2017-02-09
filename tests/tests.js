759bff69d475a337dbe82058ea913bc464b58855
umeononihu adaobi <adaumeh2003@yahoo.com>
2017-02-09 22:25:30 +0100

    just commited the newer option

--- tests/tests.js
+++ tests/tests.js
@@ -29,8 +29,8 @@ describe("Fizz Buzz tests", function() {
   it("should return 7 since its indivisible by 3 and 5", function() {
     expect(myApp.fizzBuzz(7)).toBe(7);
   });
-   it("should return 0  for 0", function() {
-    expect(myApp.fizzBuzz(0)).toBe(0);
+   it("should return 0  for wrong input", function() {
+    expect(myApp.fizzBuzz(c)).toBe(0);
   });
 
 
