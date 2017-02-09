a294e48faf88fb35ba04156cdda8712a8a9e022f
umeononihu adaobi <adaumeh2003@yahoo.com>
2017-02-09 22:22:40 +0100

    just commited it

--- tests/tests.js
+++ tests/tests.js
@@ -29,6 +29,10 @@ describe("Fizz Buzz tests", function() {
   it("should return 7 since its indivisible by 3 and 5", function() {
     expect(myApp.fizzBuzz(7)).toBe(7);
   });
+   it("should return 0  for 0", function() {
+    expect(myApp.fizzBuzz(0)).toBe(0);
+  });
+
 
   it("should return 101 since its indivisible by 3 and 5", function() {
     expect(myApp.fizzBuzz(101)).toBe(101);
