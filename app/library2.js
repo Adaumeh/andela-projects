919985579f3d232591c84edbeeb043542e6ea411
umeononihu adaobi <adaumeh2003@yahoo.com>
2017-02-10 09:09:12 +0100

    added the untracked

--- app/library2.js
+++ app/library2.js
@@ -1,9 +1,12 @@
 module.exports = {
 aritGeo:function(array){
- for(var i in array){
+ var array;
+ array = [];
    if(array == []){
      return 0;
- }else if(array[1]-array[0] === array[2] - array[1]){
+ }
+ for(var i in array){
+  if(array[1]-array[0] === array[2] - array[1]){
    return 'Arithemetic';
  }else if(array[1]/array[0] === array[3]/array[2]){
    return 'Geometric';
